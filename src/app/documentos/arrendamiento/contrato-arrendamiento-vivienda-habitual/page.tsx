a999a99533ca19938ba01fd564671c54e1ca22b6
Fabrizzioxc <fabrizziozv.17@gmail.com>
2025-08-14 16:58:18 -0500

    Feat:; agregar comentarios

--- src/app/documentos/arrendamiento/contrato-arrendamiento-vivienda-habitual/page.tsx
+++ src/app/documentos/arrendamiento/contrato-arrendamiento-vivienda-habitual/page.tsx
@@ -1,3 +1,5 @@
+//src/app/documentos/arrendamiento/contrato-arrendamiento-vivienda-habitual/page.tsx
+
 import ContractForm from '@/components/ContractForm'
 
 export default function ContractFormPage() {
