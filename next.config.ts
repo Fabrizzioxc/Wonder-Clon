8010864f6e3101a97358eaae4dc20009839fe567
Fabrizzioxc <fabrizziozv.17@gmail.com>
2025-08-15 02:08:19 -0500

    Feat:  actualizar paquetes puppeteer-core + @sparticuz/chromium

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+// next.config.ts (Next 15+)
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
+}
 
-export default nextConfig;
+export default nextConfig
